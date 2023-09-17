a657396b96488e5d042ef2a2c2c3922d103b77bc
wlong1 <109259315+wlong1@users.noreply.github.com>
2023-09-17 19:21:02 -0400

    initial

---
+++ src/test/java/com/wlong/springboottododailies/SpringBootTodoDailiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlong.springboottododailies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoDailiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
